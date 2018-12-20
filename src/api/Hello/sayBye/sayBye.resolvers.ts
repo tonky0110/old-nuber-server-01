ff679e3e10015611da91c8347f3bcd0e507f536c
tonky0110 <tonky0110@gmail.com>
2018-12-20 23:20:36 +0900

    1.12 Graphql을 위한 type등록(User)

--- src/api/Hello/sayBye/sayBye.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-  Query: {
-    sayBye: () => "Hey say Bye~"
-  }
-};
-
-export default resolvers;
--- src/api/Hello/sayHello/sayHello.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-    Query: {
-        sayHello: () => "Hey hello how are you"
-    }
-}
-
-export default resolvers;
