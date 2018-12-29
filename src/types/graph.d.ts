cdcb38e06cc03fbf1fa6b2d41d9537c1d760c383
tonky0110 <tonky0110@gmail.com>
2018-12-29 17:50:56 +0900

    1.32 email signin(login) email로 사용자 계정 존재여부 확인.

--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,27 +1,16 @@
-export const typeDefs = ["type Chat {\n  id: Int!\n  messages: [Message]!\n  participants: [User]!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  chat: Chat\n  message: Message\n  place: Place\n  ride: Ride\n  user: User\n  verification: Verification\n}\n\ntype Message {\n  id: Int!\n  text: String!\n  chat: Chat!\n  user: User!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAddress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAddress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  driver: User!\n  passenger: User!\n  duration: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype FacebookConnectResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  FacebookConnect(firstName: String!, lastName: String!, email: String, fbId: String!): FacebookConnectResponse!\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  nastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  chat: Chat\n  messages: [Message]\n  verifications: [Verification]\n  fbId: String\n  ridesAsPassenger: [Ride]\n  ridesAsDriver: [Ride]\n  createAt: String!\n  updatedAt: String\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  user: User!\n  createdAt: String!\n  updatedAt: String\n}\n"];
+export const typeDefs = ["type Chat {\n  id: Int!\n  messages: [Message]\n  passengerId: Int!\n  passenger: User!\n  driverId: Int\n  driver: User!\n  rideId: Int\n  ride: Ride!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Message {\n  id: Int!\n  text: String!\n  chatId: Int\n  chat: Chat!\n  user: User!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  userId: Int!\n  user: User!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Ride {\n  id: Int!\n  status: String!\n  pickUpAddress: String!\n  pickUpLat: Float!\n  pickUpLng: Float!\n  dropOffAddress: String!\n  dropOffLat: Float!\n  dropOffLng: Float!\n  price: Float!\n  distance: String!\n  duration: String!\n  driverId: Int\n  driver: User!\n  passengerId: Int!\n  passenger: User!\n  chat: Chat\n  chatId: Int\n  createdAt: String!\n  updatedAt: String\n}\n\ntype EmailSignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype Mutation {\n  emailSignIn(email: String!, password: String!): EmailSignInResponse!\n  FacebookConnect(firstName: String!, lastName: String!, email: String, fbId: String!): FacebookConnectResponse!\n}\n\ntype FacebookConnectResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n\ntype User {\n  id: Int!\n  email: String\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n  fbId: String\n  messages: [Message]\n  ridesAsPassenger: [Ride]\n  ridesAsDriver: [Ride]\n  chatsAsDriver: [Chat]\n  chatsAsPassenger: [Chat]\n  places: [Place]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Verification {\n  id: Int!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  user: User!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype Query {\n  verification: Verification\n}\n"];
 /* tslint:disable */
 
 export interface Query {
-  chat: Chat | null;
-  message: Message | null;
-  place: Place | null;
-  ride: Ride | null;
-  user: User | null;
   verification: Verification | null;
 }
 
-export interface Chat {
+export interface Verification {
   id: number;
-  messages: Array<Message>;
-  participants: Array<User>;
-  createdAt: string;
-  updatedAt: string | null;
-}
-
-export interface Message {
-  id: number;
-  text: string;
-  chat: Chat;
+  target: string;
+  payload: string;
+  key: string;
+  used: boolean;
   user: User;
   createdAt: string;
   updatedAt: string | null;
@@ -32,7 +21,7 @@ export interface User {
   email: string | null;
   verifiedEmail: boolean;
   firstName: string;
-  nastName: string;
+  lastName: string;
   age: number | null;
   password: string | null;
   phoneNumber: string | null;
@@ -45,23 +34,36 @@ export interface User {
   lastLng: number | null;
   lastLat: number | null;
   lastOrientation: number | null;
-  chat: Chat | null;
-  messages: Array<Message> | null;
-  verifications: Array<Verification> | null;
   fbId: string | null;
+  messages: Array<Message> | null;
   ridesAsPassenger: Array<Ride> | null;
   ridesAsDriver: Array<Ride> | null;
-  createAt: string;
+  chatsAsDriver: Array<Chat> | null;
+  chatsAsPassenger: Array<Chat> | null;
+  places: Array<Place> | null;
+  createdAt: string;
   updatedAt: string | null;
 }
 
-export interface Verification {
+export interface Message {
   id: number;
-  target: string;
-  payload: string;
-  key: string;
-  used: boolean;
+  text: string;
+  chatId: number | null;
+  chat: Chat;
   user: User;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Chat {
+  id: number;
+  messages: Array<Message> | null;
+  passengerId: number;
+  passenger: User;
+  driverId: number | null;
+  driver: User;
+  rideId: number | null;
+  ride: Ride;
   createdAt: string;
   updatedAt: string | null;
 }
@@ -76,9 +78,14 @@ export interface Ride {
   dropOffLat: number;
   dropOffLng: number;
   price: number;
-  driver: User;
-  passenger: User;
+  distance: string;
   duration: string;
+  driverId: number | null;
+  driver: User;
+  passengerId: number;
+  passenger: User;
+  chat: Chat | null;
+  chatId: number | null;
   createdAt: string;
   updatedAt: string | null;
 }
@@ -90,12 +97,20 @@ export interface Place {
   lng: number;
   address: string;
   isFav: boolean;
+  userId: number;
+  user: User;
   createdAt: string;
   updatedAt: string | null;
 }
 
 export interface Mutation {
+  emailSignIn: EmailSignInResponse;
   FacebookConnect: FacebookConnectResponse;
+}
+
+export interface EmailSignInMutationArgs {
+  email: string;
+  password: string;
 }
 
 export interface FacebookConnectMutationArgs {
@@ -103,6 +118,12 @@ export interface FacebookConnectMutationArgs {
   lastName: string;
   email: string | null;
   fbId: string;
+}
+
+export interface EmailSignInResponse {
+  ok: boolean;
+  error: string | null;
+  token: string | null;
 }
 
 export interface FacebookConnectResponse {
