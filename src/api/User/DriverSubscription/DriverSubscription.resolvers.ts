bf5e87ce459f919b7f49caeb370a34cb3e2dd8b2
tonky0110 <tonky0110@gmail.com>
2019-01-01 19:15:59 +0900

    1.70 채널로 전송된 공지들을 필터링할 수 는 없음. 유저의 상태에 따라 공지를 전송받는 여부가 달라지면 필터할 수 있음(withFilter(asyncIterator, filterFn)). withFilter기능을 이용하면, payload를 받아서 이 payload가 사용자에게 전달될지를 선택할 수 있음(filterFn - true/false) payload: ReportMovement context: DriverSubscription

--- src/api/User/DriverSubscription/DriverSubscription.resolvers.ts
+++ src/api/User/DriverSubscription/DriverSubscription.resolvers.ts
@@ -1,10 +1,30 @@
+import { withFilter } from "graphql-yoga";
+import User from "src/entities/User";
+
 // Uber고객(승객)이 주면의 drivers를 요청하는 resolver
 const resolvers = {
   Subscription: {
     DriverSubscription: {
-      subscribe: (_, __, { pubSub }) => {
-        return pubSub.asyncIterator("driverUpdate"); // 채널.
-      }
+      subscribe: withFilter(
+        (_, __, { pubSub }) => pubSub.asyncIterator("driverUpdate"),
+        (payload, _, { context }) => {
+          const user: User = context.currentUser;
+          const {
+            DriverSubscription: {
+              lastLat: driverLastLat,
+              lastLng: driverLastLng
+            }
+          } = payload;
+          const { lastLat: userLastLat, lastLng: userLastLng } = user;
+
+          return (
+            driverLastLat >= userLastLat - 0.05 &&
+            driverLastLat <= userLastLat + 0.05 &&
+            driverLastLng >= userLastLng - 0.05 &&
+            driverLastLng <= userLastLng + 0.05
+          );
+        }
+      )
     }
   }
 };
