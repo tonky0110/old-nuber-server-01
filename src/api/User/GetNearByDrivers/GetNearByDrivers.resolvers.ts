a6c1479656e408a983a025c1e7f2dcb3cf7e0f2d
tonky0110 <tonky0110@gmail.com>
2019-01-01 13:40:03 +0900

    1.64 GetNearDrivers에서 Between과 같은 operator를 사용하기 위해서는 getRepository(엔티티).find()를 사용하여 조회해야 한다.

--- src/api/User/GetNearByDrivers/GetNearByDrivers.resolvers.ts
+++ src/api/User/GetNearByDrivers/GetNearByDrivers.resolvers.ts
@@ -1,20 +1,34 @@
+import { Between, getRepository } from "typeorm";
 import User from "../../../entities/User";
 import { GetNearByDriversResponse } from "src/types/graph";
 import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
-import { Between } from "typeorm";
 
+// 근처의 Driver(운전자 - isDriving:true)목록을 찾는 resolver
 const resolvers: Resolvers = {
   Query: {
     GetNearByDrivers: privateResolver(
       async (_, __, { req }): Promise<GetNearByDriversResponse> => {
         const user: User = req.user;
         const { lastLat, lastLng } = user;
-        const drivers = await User.find({
-          isDriving: true,
-          lastLat: Between(lastLat - 0.05, lastLat + 0.05),
-          lastLng: Between(lastLng - 0.05, lastLng + 0.05)
-        });
+        try {
+          const drivers: User[] = await getRepository(User).find({
+            isDriving: true,
+            lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+            lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+          });
+          return {
+            ok: true,
+            error: null,
+            drivers
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            drivers: null
+          };
+        }
       }
     )
   }
